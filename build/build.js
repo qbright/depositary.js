6f66e4d65f45a89e3d59707785be09e2f5f13297
zqbright <zhengqiguang1@yy.com>
2017-05-26 18:21:39 +0800

    1

--- build/build.js
+++ build/build.js
@@ -6,5 +6,7 @@ var fs = require("fs");
 
 var result = UglifyJs.minify("../src/index.js");
 
+var resultv1 = UglifyJs.minify("../src/index-load-along.js");
 fs.writeFileSync("../dist/depositary.js", result.code);
+fs.writeFileSync("../dist/depositary-along.js", resultv1.code);
 
